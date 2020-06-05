b93d4a57710db3296843971d7c050e92689e2d55
ngjamesng <just.james.ng@gmail.com>
2020-06-05 13:11:15 -0700

    add next_link navigation

--- scraping_project.py
+++ scraping_project.py
@@ -1,19 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
-from random import choice
+from csv import writer
 
+import random
+from time import sleep
 
-response = requests.get("http://quotes.toscrape.com/")
-soup = BeautifulSoup(response.text, "html.parser")
-
-quotes = soup.select(".quote")
-
+#
+# section to scrape quotes
+#
+BASE_URL = "http://quotes.toscrape.com"
+url = ""
 quotes_list = []
-for quote in quotes:
-    author = quote.find(class_="author").text
-    text = quote.find(class_="text").text
-    href = quote.find("a")["href"]
-    quotes_list.append({"author": author, "text": text, "href": href})
 
-random_quote = choice(quotes_list)
-print(random_quote)
+while url or url is "":
+    response = requests.get(f"{BASE_URL}{url}")
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    quotes = soup.select(".quote")
+
+    for quote in quotes:
+        author = quote.find(class_="author").get_text()
+        text = quote.find(class_="text").get_text()
+        link = quote.find("a")["href"]
+
+        quotes_list.append({
+            "author": author,
+            "text": text,
+            "link": link
+        })
+
+    next_link = soup.find(class_="next")
+    url = next_link.find("a")["href"] if next_link else None
+
+    # seconds = round(random.uniform(1.0, 5.0), 2)
+    # sleep(seconds)
+
+print(len(quotes_list))
+
+
+#
+# Section to write to CSV file
+#
+
+# with open("quote_data.csv", "w") as csv_file:
+#     csv_writer = writer(csv_file)
+#     csv_writer.writerow(["author", "text", "link"])
+
+#     for quote in quotes_list:
+#         print(quote)
+#         csv_writer.writerow([
+#             quote["author"],
+#             quote["text"],
+#             quote["link"]
+#         ])
+
+
